8d81a7a7a8fe013ab4f11511e88a95456f882559
Havva AVCI <havvabuyukyalcin@gmail.com>
2026-06-06 19:50:12 +0200

    feat:added calculator test case under qa branch

--- src/test/java/techproed/day01/C01_SetupDriver.java
+++ src/test/java/techproed/day01/C01_SetupDriver.java
@@ -33,6 +33,41 @@ public class C01_SetupDriver {
         URL url = new URL("http://127.0.0.1:4723/");
         AndroidDriver driver = new AndroidDriver(url, options);
     }
+    @Test
+    public void testSamsungHesapMakinesi() throws MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("Android");
+        options.setAutomationName("UiAutomator2");
+        options.setUdid("988a1642334c524b5030");
+        // Samsung Hesap Makinesi bilgileri
+        options.setCapability("appPackage", "com.sec.android.app.popupcalculator");
+        // CMD ekranında eğik çizgiden sonra ne yazıyorsa aynen buraya yapıştır:
+        options.setCapability("appActivity", "com.sec.android.app.popupcalculator.Calculator");
+
+        options.setCapability("uiautomator2ServerInstallTimeout", 60000);
+
+        URL url = new URL("http://127.0.0.1:4723/");
+        AndroidDriver driver = new AndroidDriver(url, options);
+        driver.quit();
+
+    }
+    @Test
+    public void testCalculator() throws MalformedURLException, InterruptedException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("Android");
+        options.setAutomationName("UiAutomator2");
+        options.setUdid("988a1642334c524b5030");
+        options.setAppPackage("com.samsung.android.calculator");   // kendi package'ını yaz
+        options.setAppActivity("com.samsung.android.calculator.Calculator"); // kendi activity'ni yaz
+
+        URL url = new URL("http://127.0.0.1:4723/");
+        AndroidDriver driver = new AndroidDriver(url, options);
+
+        Thread.sleep(2000); // uygulama açılsın
+        System.out.println("Hesap makinesi açıldı!");
+        driver.quit();
+    }
+
 
 
 }
